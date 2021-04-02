fd09877f6e9177c3a0118d8006330f80e83d8b45
24zeroes <vdmbat@gmail.com>
2021-04-02 22:05:20 +0300

    Configuration Viper lib added

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/24zeroes/dmon/pkg/handler"
 	"github.com/24zeroes/dmon/pkg/repository"
 	"github.com/24zeroes/dmon/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(dmon.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
